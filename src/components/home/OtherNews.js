b91a52aba172a56df4feb99b967d290a1b979c8e
smoisset <smoisset@worldremit.com>
2017-09-17 22:06:27 +0100

    JSX for Home Other News

--- src/components/home/OtherNews.js
+++ src/components/home/OtherNews.js
@@ -1,10 +1,46 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
+const showOther = ({otherNews}) => {
+    if (otherNews) {
+        return otherNews.map((news) => {
+            return (
+                <Link
+                    key={news.id}
+                    to={`/news/${news.id}`}
+                    className="news">
+                    <div
+                        className="left"
+                        style={{ background: `url(/images/articles/${news.img})` }}>
+                    </div>
+                    <div className="right">
+                        <h3>{news.title}</h3>
+                        <div className="category-tag">{news.category}</div>
+                        <div className="tags">
+                            <span>
+                                <i className="fa fa-eye"></i>
+                                {news.views}
+                            </span> 
+                            <span>
+                                <i className="fa fa-thumbs-up"></i>
+                                {news.likes[0]}
+                            </span> 
+                        </div>
+                    </div>
+                </Link>
+            )
+        })
+    }
+}
+
 const OtherNews = (props) => {
-    console.log(props)
     return (
-        <div>OTHER NEWS</div>
+        <div className="other-news">
+            <h2>Other News</h2>
+            <div className="other-news-items">
+                {showOther(props)}    
+            </div>
+        </div>
     )
 }
 
