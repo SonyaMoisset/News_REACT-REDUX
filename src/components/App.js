343e947364dc6c28b0303971aadb14244266295c
smoisset <smoisset@worldremit.com>
2017-09-17 19:27:29 +0100

    Set up routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+
+import Home from '../containers/Home'
 
 class App extends Component {
     render() {
         return (
-            <div>
-                Hello world
-            </div>
+            <BrowserRouter>
+                <div>
+                    <Switch>
+                        <Route exact path="/" component={Home} />    
+                    </Switch>
+                </div>
+            </BrowserRouter>
         );
     }
 }
