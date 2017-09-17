a355a5faeb4fa513753de816e593ffea41a7bbf2
smoisset <smoisset@worldremit.com>
2017-09-17 20:08:08 +0100

    JSX for Home Latest News

--- src/components/home/Latest.js
+++ src/components/home/Latest.js
@@ -1,8 +1,33 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+
+const showLatest = ({latest}) => {
+    if (latest) {
+        return latest.map((article) => {
+            return (
+                <Link
+                    key={article.id}
+                    to={`/news/${article.id}`}
+                    className="article">
+                    <div
+                        className="image-cover"
+                        style={{ background: `url(/images/articles/${article.img})` }}>
+                        <div className="description">
+                            <span>{article.category}</span>
+                            <div>{article.title}</div>
+                        </div>
+                    </div>
+                </Link>
+            )
+        })
+    }
+}
 
 const Latest = (props) => {
     return (
-        <div>FEATURED NEWS</div>
+        <div className="home-latest">
+            {showLatest(props)}    
+        </div>
     )
 }
 
