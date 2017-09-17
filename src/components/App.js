effaca59b06045805a2c01f7ef5eabb5e22963ad
smoisset <smoisset@worldremit.com>
2017-09-17 19:34:04 +0100

    Add header and footer components

--- src/components/App.js
+++ src/components/App.js
@@ -3,14 +3,19 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 import Home from '../containers/Home'
 
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+
 class App extends Component {
     render() {
         return (
             <BrowserRouter>
                 <div>
+                    <Header />    
                     <Switch>
                         <Route exact path="/" component={Home} />    
                     </Switch>
+                    <Footer />
                 </div>
             </BrowserRouter>
         );
