85dec455998992ac8340ba18bbb50d5cfc8d4b72
smoisset <smoisset@worldremit.com>
2017-09-19 22:06:11 +0100

    JSX for Home Gallery

--- src/components/home/Gallery.js
+++ src/components/home/Gallery.js
@@ -1,9 +1,45 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import Slider from 'react-slick'
+
+const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slideToScroll: 1
+}
+
+const showGallery = ({latestGallery}) => {
+    if (latestGallery) {
+        return (
+            <Slider {...settings}>
+                {latestGallery.map((item) => {
+                    return (
+                        <Link
+                            to={`/galleries/${item.id}`}
+                            key={item.id}
+                            className="slider-item">
+                            <div
+                                className="image"
+                                style={{ background: `url(/images/galleries/${item.images[0].img})` }}>
+                                <h3>{item.artist}</h3>
+                            </div>
+                        </Link>
+                    )
+                })}    
+            </Slider>
+        )
+
+    }
+}
 
 const Gallery = (props) => {
     return (
-        <div>Gallery</div>
+        <div className="home-gallery">
+            <h2>Awesome Gallery</h2>   
+            {showGallery(props)}
+        </div>
     )
 }
 
