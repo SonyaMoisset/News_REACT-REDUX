34744ca62775db7af3bd59dab961237480f79ce7
smoisset <smoisset@worldremit.com>
2017-09-27 08:50:23 +0100

    Create Likes & Dislikes widget - Part 2

--- src/containers/LikesCounter.js
+++ src/containers/LikesCounter.js
@@ -1,18 +1,29 @@
 import React, { Component } from 'react';
 
 class LikesCounter extends Component {
+
+    addLikes = (action) => {
+        const props = this.props;
+
+        const newLikes = action === 'ADD' ? [props.likes + 1, props.dislikes] : [props.likes, props.dislikes + 1]
+    }
+
     render() {
         return (
             <div className="addlikes-wrapper">
                 <h3>How do you feel about this</h3>
                 <div className="addlikes-container">
-                    <div className="btn like">
+                    <div
+                        className="btn like"
+                        onClick={() => this.addLikes('ADD')}>
                         <div className="hits">{this.props.likes}</div>
                         <div className="icon">
                             <i className="fa fa-thumbs-up"></i>
                         </div>
                     </div>
-                    <div className="btn dislike">
+                    <div
+                        className="btn dislike"
+                        onClick={() => this.addLikes('REMOVE')}>
                         <div className="hits">{this.props.dislikes}</div>
                         <div className="icon">
                             <i className="fa fa-thumbs-down"></i>
